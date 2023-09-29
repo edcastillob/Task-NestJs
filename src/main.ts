b14167628de20924aa4da374c07326517582da7e
edcastillob <edwar.castillo@gmail.com>
2023-09-29 18:15:30 -0400

    Cors Configuracion del proyecto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { CORS } from './constants';
 
 async function server() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(morgan('dev'))
+  const configService = app.get(ConfigService)
+
+  app.enableCors(CORS);
+  
+  app.setGlobalPrefix('api');
+
+  await app.listen(configService.get("PORT"));
+  
+  console.log(`⚡📱Aplication running  on  port ${await (configService.get("PORT"))} ...`)
 }
 server();
